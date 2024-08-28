94ca95f3e52a1929e4843dc0c84a37dae87ea7bd
andrewtran28 <tran.andrew@outlook.com>
2024-08-28 14:00:59 -0400

    Add footer and credit links

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
-const pwd = document.querySelector("#password");
-const confirm_pwd = document.querySelector("#confirm-password");
+const pwd_input = document.querySelector("#password");
+const confirm_pwd_input = document.querySelector("#confirm-password");
 const pwd_msg = document.querySelector(".message");
+const form = document.querySelector("form");
 
+let invalid = "2px solid rgb(221, 0, 0)"
 
-if (pwd != confirm_pwd) {
-    pwd_msg.style.color= "red";
-    pwd_msg.textContent = "Passwords do not match.";
-}
+form.addEventListener("submit", e => {
+    const pwd = document.querySelector('#password').value;
+    const confirm_pwd = document.querySelector('#confirm-password').value;
+    const pwd_msg = document.querySelector('.pwd-msg'); 
+
+    if (pwd != confirm_pwd) {
+        pwd_input.style.border = invalid;
+        confirm_pwd_input.style.border = invalid;
+        pwd_msg.style.color = "red";
+        pwd_msg.textContent = "Passwords do not match.";
+        e.preventDefault();
+    }
+})
